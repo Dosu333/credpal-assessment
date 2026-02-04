74818f9c73c4199164fa2b70380997c2a4650d0f
Dosu333 <vladamir1865@gmail.com>
2026-02-04 14:30:22 +0100

    Add @nestjs/config dependency and integrate TypeORM for User and OtpLog entities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
+import { OtpLog } from './entities/otp-log.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, OtpLog]),
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
